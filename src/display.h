b061080fd9781c634faa0f7d830a99f22827e1c7
ivan <ivan.fourie@gmail.com>
2021-12-27 14:36:14 +0200

    Fix include

--- src/display.h
+++ src/display.h
@@ -1,16 +1,17 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
-SDL_Window* window;
-SDL_Renderer* renderer;
-uint32_t* color_buffer;
-SDL_Texture* color_buffer_texture;
-int window_width;
-int window_height;
+extern SDL_Window* window;
+extern SDL_Renderer* renderer;
+extern uint32_t* color_buffer;
+extern SDL_Texture* color_buffer_texture;
+extern int window_width;
+extern int window_height;
 
 bool initialize_window(void);
 void draw_grid(void);
