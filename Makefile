7f45dcffd4fd9ba6005d42bb463622c811016679
ivan <ivan.fourie@gmail.com>
2022-01-04 14:30:33 +0200

    Add vector rotation

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	gcc -Wall -std=c99 ./src/*.c -lSDL2 -o rendeder
+	gcc -Wall -std=c99 ./src/*.c -lSDL2 -lm -o rendeder
 
 run:
 	./rendeder
--- src/vector.h
+++ src/vector.h
@@ -12,7 +12,9 @@ typedef struct {
 	float z;
 } vec3_t;
 
-// TODO: Add functions to manipulate 2d and 3d vectors
-// ...
+// Functions to manipulate 2d and 3d vectors
+vec3_t vec3_rotate_x(vec3_t v, float angle);
+vec3_t vec3_rotate_y(vec3_t v, float angle);
+vec3_t vec3_rotate_z(vec3_t v, float angle);
 
 #endif
