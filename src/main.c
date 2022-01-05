c4821ce1cbf1a0ad151be49c6b970802643967c4
ivan <ivan.fourie@gmail.com>
2022-01-05 11:26:10 +0200

    Implement dynamic mesh vertices and faces

--- src/main.c
+++ src/main.c
@@ -10,7 +10,6 @@
 triangle_t* triangles_to_render = NULL;
 
 vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
-vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
 
 float fov_factor = 640;
 
@@ -29,6 +28,9 @@ void setup(void) {
         window_width,
         window_height
     );
+
+    // Loads the cube values in the mesh data structure
+    load_cube_mesh_data();
 }
 
 
@@ -69,18 +71,20 @@ void update(void) {
     // Initialise array of triangles to render
     triangles_to_render = NULL;
     
-    cube_rotation.x += 0.01;
-    cube_rotation.y += 0.01;
-    cube_rotation.z += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
     
     // Loop all triangle faces of our mesh
-    for (int i = 0; i < M_MESH_FACES; i++) {
-        face_t mesh_face = mesh_faces[i];
+    int num_faces = array_length(mesh.faces);
+
+    for (int i = 0; i < num_faces; i++) {
+        face_t mesh_face = mesh.faces[i];
         
         vec3_t face_vertices[3];
-        face_vertices[0] = mesh_vertices[mesh_face.a - 1];
-        face_vertices[1] = mesh_vertices[mesh_face.b - 1];
-        face_vertices[2] = mesh_vertices[mesh_face.c - 1];
+        face_vertices[0] = mesh.vertices[mesh_face.a - 1];
+        face_vertices[1] = mesh.vertices[mesh_face.b - 1];
+        face_vertices[2] = mesh.vertices[mesh_face.c - 1];
 
         triangle_t projected_triangle;
 
@@ -88,9 +92,9 @@ void update(void) {
         for (int j = 0; j < 3; j++) {
             vec3_t transformed_vertex = face_vertices[j];
 
-            transformed_vertex = vec3_rotate_x(transformed_vertex, cube_rotation.x);
-            transformed_vertex = vec3_rotate_y(transformed_vertex, cube_rotation.y);
-            transformed_vertex = vec3_rotate_z(transformed_vertex, cube_rotation.z);
+            transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
+            transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
+            transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
 
             // Translate the vertex away from the camera in z
             transformed_vertex.z -= camera_position.z;
@@ -116,8 +120,9 @@ void render(void) {
 
     draw_grid();
 
-    // Loop all projected point and render them
-    for (int i = 0; i < M_MESH_FACES; i++) {
+    // Loop all projected triangles and render them
+    int num_triangles = array_length(triangles_to_render);
+    for (int i = 0; i < num_triangles; i++) {
         triangle_t triangle = triangles_to_render[i];
         
         // Draw vertex points
@@ -147,6 +152,12 @@ void render(void) {
     SDL_RenderPresent(renderer);
 }
 
+void free_resources(void) {
+    free(color_buffer);
+    array_free(mesh.faces);
+    array_free(mesh.vertices);
+}
+
 int main(int argc, char* args[]) {
     
     // Create a SDL window
@@ -162,6 +173,7 @@ int main(int argc, char* args[]) {
     }
 
     destroy_window();
+    free_resources();
 
     return 0;
 }
--- src/mesh.h
+++ src/mesh.h
@@ -4,10 +4,21 @@
 #include "vector.h"
 #include "triangle.h"
 
-#define M_MESH_VERTICES 8
-extern vec3_t mesh_vertices[M_MESH_VERTICES];
+#define M_CUBE_VERTICES 8
+#define M_CUBE_FACES (6 * 2) // 6 cube faces, 2 traingles per face
 
-#define M_MESH_FACES (6 * 2) // 6 cube faces, 2 traingles per face
-extern face_t mesh_faces[M_MESH_FACES];
+extern vec3_t cube_vertices[M_CUBE_VERTICES];
+extern face_t cube_faces[M_CUBE_FACES];
+
+// Define a struct for dynamic size meshes
+typedef struct {
+	vec3_t* vertices;	// dynamic array of vertices
+	face_t* faces;		// dynamic array of faces
+	vec3_t rotation;	// rotation with x, y, z values
+} mesh_t;
+
+extern mesh_t mesh;
+
+void load_cube_mesh_data(void);
 
 #endif
