2c84f5aa8b6a66f67ea105fc15b0117d20634e60
ivan <ivan.fourie@gmail.com>
2022-01-06 20:38:13 +0200

    Add vector operations

--- src/vector.c
+++ src/vector.c
@@ -1,7 +1,115 @@
 #include <math.h>
 #include "vector.h"
 
+//
+// Implementations of Vector 2D functions
+//
 
+// vec2 length (vector magninitude) using Pythagoras theorem on x, y
+float vec2_length(vec2_t v) {
+	return sqrt(v.x * v.x + v.y * v.y);
+}
+
+vec2_t vec2_add(vec2_t a, vec2_t b) {
+	vec2_t result = {
+		.x = a.x + b.x,
+		.y = a.y + b.y
+	};
+	return result;
+}
+
+vec2_t vec2_sub(vec2_t a, vec2_t b) {
+	vec2_t result = {
+		.x = a.x - b.x,
+		.y = a.y - b.y
+	};
+	return result;
+}
+
+vec2_t vec2_mul(vec2_t v, float factor) {
+	vec2_t result = {
+		.x = v.x * factor,
+		.y = v.y * factor
+	};
+	return result;
+}
+
+vec2_t vec2_div(vec2_t v, float factor) {
+	vec2_t result = {
+		.x = v.x / factor,
+		.y = v.y / factor
+	};
+	return result;
+}
+
+// Calculate dot product between two 2D vectors (used to determine vector alignment)
+float vec2_dot(vec2_t a, vec2_t b) {
+	return (a.x * b.x) + (a.y * b.y);
+}
+
+
+//
+// Implementations of Vector 3D functions
+//
+
+// vec3 length (vector magninitude) using Pythagoras theorem on x, y and z
+float vec3_length(vec3_t v) {
+	return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+vec3_t vec3_add(vec3_t a, vec3_t b) {
+	vec3_t result = {
+		.x = a.x + b.x,
+		.y = a.y + b.y,
+		.z = a.z + b.z
+	};
+	return result;
+}
+
+vec3_t vec3_sub(vec3_t a, vec3_t b) {
+	vec3_t result = {
+		.x = a.x - b.x,
+		.y = a.y - b.y,
+		.z = a.z - b.z
+	};
+	return result;
+}
+
+vec3_t vec3_mul(vec3_t v, float factor) {
+	vec3_t result = {
+		.x = v.x * factor,
+		.y = v.y * factor,
+		.z = v.z * factor
+	};
+	return result;
+}
+
+vec3_t vec3_div(vec3_t v, float factor) {
+	vec3_t result = {
+		.x = v.x / factor,
+		.y = v.y / factor,
+		.z = v.z / factor
+	};
+	return result;
+}
+
+// Calculate cross product between two vectors (used to determine vector normal used to get ray that is perpendicular to an object)
+vec3_t vec3_cross(vec3_t a, vec3_t b) {
+	vec3_t result = {
+		.x = a.y * b.z - a.z * b.y,
+		.y = a.z * b.y - a.x * b.z,
+		.z = a.x * b.y - a.y * b.z
+	};
+	return result;
+}
+
+// Calculate dot product between two 3D vectors (used to determine vector alignment)
+// If it is positive they are pointing in the same direction, 0 they are perpendicular and negative away from each other
+float vec3_dot(vec3_t a, vec3_t b) {
+	return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+}
+
+// Rotate around x rotation based on the angle given using sine and cosine angle addition formulas to calculate rotation
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
 	vec3_t rotated_vector = {
 		.x = v.x,
@@ -11,6 +119,7 @@ vec3_t vec3_rotate_x(vec3_t v, float angle) {
 	return rotated_vector;
 }
 
+// Rotate around y rotation based on the angle given using sine and cosine angle addition formulas to calculate rotation
 vec3_t vec3_rotate_y(vec3_t v, float angle) {
 	vec3_t rotated_vector = {
 		.x = v.x * cos(angle) - v.z * sin(angle),
@@ -21,6 +130,7 @@ vec3_t vec3_rotate_y(vec3_t v, float angle) {
 
 }
 
+// Rotate around z rotation based on the angle given using sine and cosine angle addition formulas to calculate rotation
 vec3_t vec3_rotate_z(vec3_t v, float angle) {
 	vec3_t rotated_vector = {
 		.x = v.x * cos(angle) - v.y * sin(angle),
