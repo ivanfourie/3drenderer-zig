35ebd7c9f62caf555854b2ddc00813e5ce65822b
ivan <ivan.fourie@gmail.com>
2022-01-06 21:33:15 +0200

    Fix cross product calculation

--- src/vector.c
+++ src/vector.c
@@ -97,8 +97,8 @@ vec3_t vec3_div(vec3_t v, float factor) {
 vec3_t vec3_cross(vec3_t a, vec3_t b) {
 	vec3_t result = {
 		.x = a.y * b.z - a.z * b.y,
-		.y = a.z * b.y - a.x * b.z,
-		.z = a.x * b.y - a.y * b.z
+		.y = a.z * b.x - a.x * b.z,
+		.z = a.x * b.y - a.y * b.x
 	};
 	return result;
 }
