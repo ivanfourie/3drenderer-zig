55761be0bf4a84c7d0fefc5894cf1e343d1b362c
ivan <ivan.fourie@gmail.com>
2022-01-15 16:16:42 +0200

    Use enums for render methods

--- src/main.c
+++ src/main.c
@@ -22,16 +22,14 @@ float fov_factor = 640;
 bool is_running = false;
 int previous_frame_time = 0;
 
-// Render modes
-bool is_draw_wireframe = true;
-bool is_draw_vertices = false;
-bool is_draw_filled = true;
-bool is_back_face_culling = true;
-
 //
 // Setup function to initialise variables and game objects
 //
 void setup(void) {
+    // Initialize render mode and triangle culling method
+    render_method = RENDER_WIRE;
+    cull_method = CULL_BACKFACE;
+    
     // Allocate the required memory in bytes for the color buffer
     color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 
@@ -69,35 +67,32 @@ void process_input(void) {
                 case SDLK_1:
                     // Display the wireframe and a small red dot for each triangle vertex
                     printf("Mode: Show the wireframe and a small red dot for each triangle vertex.\n");
-                    is_draw_wireframe = true;
-                    is_draw_vertices = true;
-                    is_draw_filled = false;
+                    render_method = RENDER_WIRE_VERTEX;
                     break;
                 case SDLK_2:
                     // Display only the wireframe lines
                     printf("Mode: Show only the wireframe lines.\n");
-                    is_draw_wireframe = true;
-                    is_draw_vertices = false;
-                    is_draw_filled = false;
+                    render_method = RENDER_WIRE;
                     break;
                 case SDLK_3:
                     // Display filled triangles with a solid color
                     printf("Mode: Show filled triangles with a solid color.\n");
-                    is_draw_wireframe = false;
-                    is_draw_vertices = false;
-                    is_draw_filled = true;
+                    render_method = RENDER_FILL_TRIANGLE;
                     break;
                 case SDLK_4:
                     // Display both filled triangles and wireframe lines
                     printf("Mode: Show both filled triangles and wireframe lines.\n");
-                    is_draw_wireframe = true;
-                    is_draw_vertices = false;
-                    is_draw_filled = true;
+                    render_method = RENDER_FILL_TRIANGLE_WIRE;
                     break;
                 case SDLK_c:
                     // Toggle back-face culling
-                    printf("Mode: Back-face culling %s.\n", !is_back_face_culling ? "on" : "off");
-                    is_back_face_culling = !is_back_face_culling;
+                    if (cull_method == CULL_BACKFACE) {
+                        cull_method = CULL_NONE;
+                    } else {
+                        cull_method = CULL_BACKFACE;
+                    }
+                    printf("Mode: Back-face culling %s.\n", cull_method == CULL_BACKFACE ? "on" : "off");
+                    
                     break;
             }
             break;
@@ -165,34 +160,37 @@ void update(void) {
             transformed_vertices[j] = transformed_vertex;
         }
         
-        // Check backface culling
-        vec3_t vector_a = transformed_vertices[0]; /*   A   */
-        vec3_t vector_b = transformed_vertices[1]; /*  / \  */
-        vec3_t vector_c = transformed_vertices[2]; /* C---B */
+        // Backface culling test to see if the current face should be projected
+        if (cull_method == CULL_BACKFACE) {
+            // Check backface culling
+            vec3_t vector_a = transformed_vertices[0]; /*   A   */
+            vec3_t vector_b = transformed_vertices[1]; /*  / \  */
+            vec3_t vector_c = transformed_vertices[2]; /* C---B */
 
-        // Get the vector subtraction (B-A) and (C-A)
-        vec3_t vector_ba = vec3_sub(vector_b, vector_a);
-        vec3_t vector_ca = vec3_sub(vector_c, vector_a);
-        // Normalize to normal vectors
-        vec3_normalize(&vector_ba);
-        vec3_normalize(&vector_ca);
+            // Get the vector subtraction (B-A) and (C-A)
+            vec3_t vector_ba = vec3_sub(vector_b, vector_a);
+            vec3_t vector_ca = vec3_sub(vector_c, vector_a);
+            // Normalize to normal vectors
+            vec3_normalize(&vector_ba);
+            vec3_normalize(&vector_ca);
 
-        // Compute the face normal (using cross product to find perpendicular)
-        // because the coordinate system is left handed the cross product will (AB cross CA)
-        vec3_t normal = vec3_cross(vector_ba, vector_ca);
+            // Compute the face normal (using cross product to find perpendicular)
+            // because the coordinate system is left handed the cross product will (AB cross CA)
+            vec3_t normal = vec3_cross(vector_ba, vector_ca);
 
-        // Normalize the face normal vector
-        vec3_normalize(&normal);
+            // Normalize the face normal vector
+            vec3_normalize(&normal);
 
-        // Find the the vector between a point in the triangle and the camera origin
-        vec3_t camera_ray = vec3_sub(camera_position, vector_a);
+            // Find the the vector between a point in the triangle and the camera origin
+            vec3_t camera_ray = vec3_sub(camera_position, vector_a);
 
-        // Calculate how aligned the camera ray is with the dot normal (using dot product)
-        float dot_normal_camera = vec3_dot(normal, camera_ray);
+            // Calculate how aligned the camera ray is with the dot normal (using dot product)
+            float dot_normal_camera = vec3_dot(normal, camera_ray);
 
-        if (is_back_face_culling && dot_normal_camera < 0)
             // Bypass triangle that are looking away from the camera
-            continue;
+            if (dot_normal_camera < 0)
+                continue;
+        }
 
         triangle_t projected_triangle;
 
@@ -227,30 +225,31 @@ void render(void) {
     for (int i = 0; i < num_triangles; i++) {
         triangle_t triangle = triangles_to_render[i];
         
-        if (is_draw_vertices) {
+        if (render_method == RENDER_WIRE_VERTEX) {
         // // Draw vertex points
-            draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFF0000); // vertex A
-            draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFF0000); // vertex B
-            draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFF0000); // vertex C
+            draw_rect(triangle.points[0].x - 3, triangle.points[0].y - 3, 6, 6, 0xFFFF0000); // vertex A
+            draw_rect(triangle.points[1].x - 3, triangle.points[1].y - 3, 6, 6, 0xFFFF0000); // vertex B
+            draw_rect(triangle.points[2].x - 3, triangle.points[2].y - 3, 6, 6, 0xFFFF0000); // vertex C
         }
 
-        if (is_draw_filled) {
+        if (render_method == RENDER_FILL_TRIANGLE || render_method == RENDER_FILL_TRIANGLE_WIRE) {
             // Draw filled triangle
             draw_filled_triangle(
                 triangle.points[0].x, triangle.points[0].y, // vertex A
                 triangle.points[1].x, triangle.points[1].y, // vertex B
                 triangle.points[2].x, triangle.points[2].y, // vertex C
-                0xFFFFFFFF
+                0xFF555555
             );
         }
 
-        if (is_draw_wireframe) {
+        if (render_method == RENDER_WIRE || render_method == RENDER_WIRE_VERTEX 
+            || render_method == RENDER_FILL_TRIANGLE_WIRE) {
             // Draw unfilled triangle
             draw_triangle(
                 triangle.points[0].x, triangle.points[0].y, // vertex A
                 triangle.points[1].x, triangle.points[1].y, // vertex B
                 triangle.points[2].x, triangle.points[2].y, // vertex C
-                0xFF0000FF
+                0xFFFFFFFF
             );
         }
 
